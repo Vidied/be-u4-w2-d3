65da187fb57ca0be569d1fafc4df30d22327973d
Vidied <davidepan2001@gmail.com>
2026-06-10 14:54:52 +0200

    scheletro delle classi creato

--- src/Entities/Customer.java
+++ src/Entities/Customer.java
@@ -1,7 +1,31 @@
 package Entities;
 
 public class Customer {
-  private long id;
+  private Long id;
   private String name;
   private  Integer tier;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getTier() {
+    return tier;
+  }
+
+  public void setTier(Integer tier) {
+    this.tier = tier;
+  }
 }
